222eba03ce7396dee6cb6eeac2ee0626050f6147
Tessa Myllys <tessam@ety-a2123b-15.edu.wks.metropolia.fi>
2018-01-19 11:45:28 +0200

    Palautus

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -9,6 +9,12 @@ import {HttpClient} from '@angular/common/http';
 export class HttpTestComponent implements OnInit {
 
     tulos = 'Moro';
+    apitulos = 'Moro taas';
+    apiosoite = 'http://media.mw.metropolia.fi/wbma';
+    kuvaosoite = 'http://media.mw.metropolia.fi/wbma/uploads/';
+    autotulos = 'Jej';
+    autoosoite = 'https://api.openchargemap.io/v2/poi/';
+    tulokset : any;
 
     constructor(private http: HttpClient) {
     }
@@ -24,8 +30,27 @@ export class HttpTestComponent implements OnInit {
         });
     }
 
+    getFromApi() {
+        this.http.get(this.apiosoite + '/media').subscribe(data => {
+            console.log(data[0].filename);
+            this.apitulos = this.kuvaosoite + data[0].filename;
+        });
+    };
+
+    getFromAutoApi() {
+        interface Autointerface {
+            ID: number;
+        }
+        this.http.get(this.autoosoite).subscribe(data => {
+            console.log(data[0].ID);
+            this.autotulos = this.autoosoite + data[0].ID;
+        });
+    }
+
     ngOnInit() {
-        this.getJson();
+        //this.getJson();
+        //this.getFromApi();
+        this.getFromAutoApi();
     }
 
 }
